0755493580f8b2b8caad1622d89c22e44af39a4e
naseef434 <naseefp.hin@gmail.com>
2020-11-08 11:03:31 +0530

    Supplier crud operation completed

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.warehouse_login),
     path('dashboard', views.dashboard),
-    path('supplier', views.supplier),
+    path('supplier', views.supplier,name='supplier'),
     path('add_supplier', views.add_supplier),
+    path('edit_supplier/<int:id>', views.edit_supplier),  
+    path('update_supplier/<int:id>', views.update_supplier), 
+    path('delete_supplier/<int:id>', views.delete_supplier), 
     
 ]
--- warehouse/views.py
+++ warehouse/views.py
@@ -11,10 +11,15 @@ def dashboard(request):
      return render(request, 'warehouse/dashboard.html')
 
 def supplier(request):
-    return render(request, 'warehouse/supplier_list.html')
+    suppliers = Supplier.objects.all()
+    return render(request, 'warehouse/supplier_list.html',{'suppliers':suppliers})
+
+
 
 def add_supplier(request):
-    if request.method == "POST":
+    if request.method == "GET":
+        return render(request, 'warehouse/add_supplier.html')
+    elif request.method == "POST":
         supplier  = Supplier()
         supplier.supplier_name = request.POST.get('supplier_name') 
         supplier.brand_name  = request.POST.get('brand')
@@ -26,6 +31,29 @@ def add_supplier(request):
         supplier.supplier_address  = request.POST.get('address')
         supplier.save()
         messages.success(request, 'Supplier Added successfully')
-        return render(request, 'warehouse/supplier_list.html')
+        return redirect('/supplier')
     
-    
+def edit_supplier(request, id):
+    supplier = Supplier.objects.get(id=id)
+    return render(request, 'warehouse/edit_supplier.html', {'supplier':supplier})
+
+def update_supplier(request, id):
+    if request.method == "POST":
+        supplier = Supplier.objects.get(id=id)
+        supplier.supplier_name = request.POST.get('supplier_name') 
+        supplier.brand_name  = request.POST.get('brand')
+        supplier.supplier_phone  = request.POST.get('phone')
+        supplier.gst_uin  = request.POST.get('gst')
+        supplier.state_name  = request.POST.get('state')
+        supplier.code  = request.POST.get('code')
+        supplier.email  = request.POST.get('email')
+        supplier.supplier_address  = request.POST.get('address')
+        supplier.save()
+        messages.warning(request, 'Supplier Updated successfully')
+        return redirect('/supplier')
+
+def delete_supplier(request, id):
+    supplier = Supplier.objects.get(id=id)  
+    supplier.delete()  
+    messages.info(request,"Supplier Deleted Successfully")
+    return redirect('/supplier')
